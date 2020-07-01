f14a0c7d3c4ce87429f53345e8318b24a24a9776
Edwin <1310912217@qq.com>
2020-07-01 15:39:13 +0800

    设计一段代码，实现逻辑。

--- javascript/wwwroot/JavaScript.js
+++ javascript/wwwroot/JavaScript.js
@@ -1,12 +1,28 @@
 ﻿alert("if you think you can .then you can.");
 
-
+//用一个变量替换“编程”，以便输出更多内容。比如：助你实现{职业}梦想、助你实现{人生}梦想 
 var sname;
 sname = "李智博";
 sname = "王胖子";
 sname = "阿泰";
 sname = "飞哥";
 
-
+//设置一个常量password，保存你的密码
 const password = "4936512"
-function
+
+//设计一段代码，实现以下逻辑
+//某用户有一定数量的帮帮豆（sum）
+//每点赞一次就会消耗若干帮帮豆（consume）
+//现在用户点赞若干次（count）
+//计算用户剩余了多少帮帮豆（banlance）并用bool值显示他是否还有帮帮豆（isAvailable）
+
+
+
+var sum = x;
+consume = y;
+banlance = (x - y);
+if (banlance > 0) {
+    console.log(true);
+} else {
+    console.log(false);
+}
